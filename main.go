b3fcdb83bf10d59cb3dd4a017d2f27d2920f9d42
NewbieCoder99 <fajarpunya99@gmail.com>
2019-11-15 23:14:16 +0700

    Add environment file

--- main.go
+++ main.go
@@ -1,15 +1,25 @@
 package main
 
-import "github.com/gin-gonic/gin"
-import "net/http"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"net/http"
+	"os"
+	"log"
+)
 
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+	  log.Fatal("Error loading .env file")
+	}
+
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
-	//router.LoadHTMLFiles("templates/template1.html", "templates/template2.html")
 	router.GET("/index", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", gin.H{
-			"title": "Main website",
+			"title": os.Getenv("APP_NAME"),
 		})
 	})
 	router.Run(":8080")
