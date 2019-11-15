b25d3ebd32a751a56f2f264aefbc1613bfe811bb
NewbieCoder99 <fajarpunya99@gmail.com>
2019-11-16 00:01:52 +0700

    remove package

--- main.go
+++ main.go
@@ -3,16 +3,16 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"log"
 	"net/http"
 	"os"
-	"log"
 )
 
 func main() {
 
 	err := godotenv.Load()
 	if err != nil {
-	  log.Fatal("Error loading .env file")
+		log.Fatal("Error loading .env file")
 	}
 
 	router := gin.Default()
